1d72afa697b4e9245f789e9c599c73bc208a8260
KLAIIFAHMI <klaiifahmigmail.com>
2022-08-17 22:22:05 +0100

    adding form + emailJs service + javascript file

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 
+function sendEmail() {
+    // console.log('it,s working');
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "klaiifahmi@gmail.com",
+        Password : "6405669FF33774F553BFA29C9AB7702126D4",
+        To : 'klaiifahmi@gmail.com',
+        From : "klaiifahmi@gmail.com",
+        Subject :"RBK student email: " + document.getElementById("subjectOption").value,
+        Body : "Name: " + document.getElementById("name").value + 
+        "<br> Email: "+document.getElementById("emailId").value +
+        "<br> Message: <br>"+document.getElementById("IdeaSubject").value
+    }).then(
+      message => alert("Thanks ! we will try to contact you as soon as possible !")
+    );
+    document.getElementById("msform").reset();
+    }
+
+ 
